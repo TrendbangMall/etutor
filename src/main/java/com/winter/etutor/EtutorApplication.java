bd4086c58d37e0e98663680dcc473d43b8ecdcfe
winter <550854519@qq.com>
2020-04-08 00:26:47 +0800

    初始化

---
+++ src/main/java/com/winter/etutor/EtutorApplication.java
@@ -0,0 +1,13 @@
+package com.winter.etutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtutorApplication.class, args);
+    }
+
+}
