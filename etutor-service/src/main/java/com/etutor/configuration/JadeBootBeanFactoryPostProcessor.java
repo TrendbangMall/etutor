4045b1b53e93fd67fe19293a84bf2c3b6440e8a6
songdongdong <songdongdong@kanzhun.com>
2020-04-09 17:07:36 +0800

    多模块 package

--- etutor-service/src/main/java/com/etutor/configuration/JadeBootBeanFactoryPostProcessor.java
+++ etutor-service/src/main/java/com/etutor/configuration/JadeBootBeanFactoryPostProcessor.java
@@ -1,6 +1,7 @@
 package com.etutor.configuration;
 
 import net.paoding.rose.jade.context.spring.JadeBeanFactoryPostProcessor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -8,4 +9,5 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class JadeBootBeanFactoryPostProcessor extends JadeBeanFactoryPostProcessor {
+
 }
